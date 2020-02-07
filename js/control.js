77332a38e978baf93be210a6ffba30b88c444437
mikhail-bodnar <mikhail.bodnar@gmail.com>
2020-02-07 16:25:17 +0200

    set

---
+++ js/control.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.container_slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    // autoplaySpeed: 8000,
+    variableWidth: true,
+    arrows: false,
+  });
+});
